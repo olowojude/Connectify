e41df6c2abec7ab8c5bbe52ebd5d50ec2a79d8bc
olowojude <olowojude73@gmail.com>
2025-01-10 01:42:00 +0100

    feat: Added styling for all the pages

--- app/models.py
+++ app/models.py
@@ -16,8 +16,8 @@ class User(AbstractUser):
 
 
 class Link(models.Model):
-    name = models.CharField(max_length=15)
-    url = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    url = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, re_path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -12,3 +14,5 @@ urlpatterns = [
     path("delete-link/<str:pk>/", views.deletelink, name="deletelink"),
     re_path(r'^(?P<username>\w+)/$', views.preview, name="preview"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
