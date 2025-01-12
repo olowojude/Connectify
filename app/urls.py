a5a7b62bd42c6330dc8907ad48d72b10cc01737b
olowojude <olowojude73@gmail.com>
2025-01-12 18:56:31 +0100

    feat: implement reset password functionality for user accounts

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 
@@ -17,6 +18,14 @@ urlpatterns = [
     path("delete-link/<str:pk>/", views.deletelink, name="deletelink"),
     re_path(r'^(?P<username>\w+)/$', views.preview, name="preview"),
     path("update-profile/", views.updateProfile, name="updateprofile"),
+    path('reset-password/', auth_views.PasswordResetView.as_view(template_name="password-reset-page.html"),
+         name="reset_password"),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="lta/password-reset-form-page.html"),
+         name="password_reset_confirm"),
+    path('reset-password-sent/', auth_views.PasswordResetDoneView.as_view(template_name="password-reset-sent-page.html"),
+         name="password_reset_done"),
+    path('reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password-reset-complete-page.html"),
+         name="password_reset_complete")
 ]
 # URL patterns to serve static files during development
 urlpatterns += staticfiles_urlpatterns()
